c411b2afc726e33e0f6f8ddfa82c146d0cf7231b
mralamdari <mralamdari2000@gmail.com>
2025-06-07 17:47:47 +0330

    added requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+#find_packages() will lookfor every construction file (__init__()) in every folder
+# and will try to install that folder as a Local Package, so we can import
+
+
+setup(
+    name= "SmartCCTVHighway",
+    version= "0.0.0",
+    author= "Esmaeil Fakheri Alamdari",
+    author_email= "mralamdari2000@gmial.com",
+    packages= find_packages(),
+    install_requires = []
+)
